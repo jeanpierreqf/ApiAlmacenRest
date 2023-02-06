5e10be8b538a736b386d8856483a8371abfb703f
jeanpierreqf <114685677+jeanpierreqf@users.noreply.github.com>
2023-02-06 18:56:36 -0500

    Primer intento

---
+++ src/main/java/pe/com/apialmacen/ApiAlmacenApplication.java
@@ -0,0 +1,13 @@
+package pe.com.apialmacen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAlmacenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAlmacenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/com/apialmacen/ApiAlmacenApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.apialmacen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAlmacenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
