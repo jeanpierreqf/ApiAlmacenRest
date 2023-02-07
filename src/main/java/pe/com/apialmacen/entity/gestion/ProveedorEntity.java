37316a390d1f0892e0da8fb8241e5f7fb660e8c9
BryanErnesto27 <117932365+BryanErnesto27@users.noreply.github.com>
2023-02-07 08:44:16 -0600

    hhhhhhhh

--- src/main/java/pe/com/apialmacen/entity/gestion/ProveedorEntity.java
+++ src/main/java/pe/com/apialmacen/entity/gestion/ProveedorEntity.java
@@ -5,6 +5,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import java.io.Serializable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
