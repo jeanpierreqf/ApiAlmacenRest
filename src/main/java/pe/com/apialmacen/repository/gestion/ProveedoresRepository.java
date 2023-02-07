8cf1e6ee10d8190bd1dc649f168c9e8fbb6876d6
Erickninguno01 <sv70993608@idat.edu.pe>
2023-02-07 08:51:43 -0500

    2
    2

--- src/main/java/pe/com/apialmacen/repository/gestion/ProveedoresRepository.java
+++ src/main/java/pe/com/apialmacen/repository/gestion/ProveedoresRepository.java
@@ -4,13 +4,14 @@ package pe.com.apialmacen.repository.gestion;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import pe.com.apialmacen.entity.gestion.ProveedorEntity;
 
 
  
-public interface ProveedoresRepository extends JpaRepository<ProveedoresEntity, Long>{
+public interface ProveedoresRepository extends JpaRepository<ProveedorEntity, Long>{
     //podemos crear query personalizados
     @Query("select c from CategoriaEntity c where c.estado=1")
-    List<ProveedoresEntity> findAllCustom();
+    List<ProveedorEntity> findAllCustom();
 }
     
 
--- src/main/java/pe/com/apialmacen/service/impl/gestion/ProveedorServiceImpl.java
+++ src/main/java/pe/com/apialmacen/service/impl/gestion/ProveedorServiceImpl.java
@@ -1,13 +1,51 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package pe.com.apialmacen.service.impl.gestion;
 
-/**
- *
- * @author frank
- */
-public class ProveedorServiceImpl {
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import pe.com.apialmacen.entity.gestion.ProveedorEntity;
+import pe.com.apialmacen.repository.gestion.ProveedoresRepository;
+import pe.com.apialmacen.service.gestion.ProveedorService;
+
+
+public class ProveedorServiceImpl implements ProveedorService{
+    @Autowired
+    private ProveedoresRepository repositorio;
+
+    @Override
+    public List<ProveedorEntity> findAll() {
+        return repositorio.findAll();
+    }
+
+    @Override
+    public List<ProveedorEntity> findAllCustom() {
+        return repositorio.findAllCustom();
+    }
+
+    @Override
+    public ProveedorEntity add(ProveedorEntity t) {
+        return repositorio.save(t);
+    }
+
+    @Override
+    public Optional<ProveedorEntity> findById(Long id) {
+        return repositorio.findById(id);
+    }
+
+    @Override
+    public ProveedorEntity update(ProveedorEntity t) {
+        ProveedorEntity objcategoria = repositorio.getById(t.getCodigo());
+        BeanUtils.copyProperties(t, objcategoria);
+        return repositorio.save(objcategoria);
+    }
+
+    @Override
+    public ProveedorEntity delete(ProveedorEntity t) {
+        ProveedorEntity objcategoria = repositorio.getById(t.getCodigo());
+        objcategoria.setEstado(false);
+        return repositorio.save(objcategoria);
+    }
     
 }
