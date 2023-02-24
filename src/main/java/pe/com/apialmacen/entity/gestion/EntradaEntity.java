761c516b9a00c2760c05502b456142c2d48927cc
JuniorCerpe <juniorcervera13@gmail.com>
2023-02-24 17:52:51 -0500

    Entrada entity

--- src/main/java/pe/com/apialmacen/entity/gestion/EntradaEntity.java
+++ src/main/java/pe/com/apialmacen/entity/gestion/EntradaEntity.java
@@ -1,5 +1,38 @@
 package pe.com.apialmacen.entity.gestion;
 
-public class EntradaEntity {
-    
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import pe.com.apialmacen.entity.base.EntityBase;
+
+@SuperBuilder 
+@NoArgsConstructor 
+@AllArgsConstructor 
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Entity(name="EntradaEntity") 
+@Table(name="entrada") 
+public class EntradaEntity extends EntityBase implements Serializable{
+    private static final long serialVersionUID=1L;
+    @Id 
+    @Column(name="codent")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long codigo;
+    @ManyToOne //relacion de uno a muchos
+    @JoinColumn(name="codpro",nullable = false)
+    private ProductoEntity producto;
+    @ManyToOne //relacion de uno a muchos
+    @JoinColumn(name="codusu",nullable = false)
+     private UsuariosEntity Usuarios;
 }
