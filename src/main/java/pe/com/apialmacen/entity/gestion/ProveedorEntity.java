ac8b568b94471b0fe1bcbe3d35ee8db94fec1d49
JuniorCerpe <juniorcervera13@gmail.com>
2023-02-24 18:52:13 -0500

    proveedor

--- src/main/java/pe/com/apialmacen/entity/gestion/ProveedorEntity.java
+++ src/main/java/pe/com/apialmacen/entity/gestion/ProveedorEntity.java
@@ -1,5 +1,63 @@
 package pe.com.apialmacen.entity.gestion;
 
-public class ProveedorEntity {
-    
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import pe.com.apialmacen.entity.base.EntityBase;
+
+@SuperBuilder 
+@NoArgsConstructor 
+@AllArgsConstructor 
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Entity(name="ProveedorEntity") 
+@Table(name="proveedor") 
+public class ProveedorEntity extends EntityBase implements Serializable{
+    private static final long serialVersionUID=1L;
+    @Id 
+    @Column(name="codprove")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long codigo;
+    @Column(name="nomprove",length = 60, nullable = false)
+    @NotEmpty(message = "Debe de ingresar el nombre")
+    @Size(min=5,max=40,message = "El nombre debe de tener como minimo {min} y maximo {max}")
+    private String nombre;
+    @Column(name="telepro",nullable = false)
+    @Positive(message = "El telefono del proveedor")//permita valores positivos
+    private int telefono;
+    @Column(name="direprove",nullable = false)
+    @Positive(message = "La direccion del proveedor")
+    private String direccion;
+    @Column(name="correprove",nullable = false)
+    @Positive(message = "Correo del proveedor")//permite valores positivos o cero
+    private int correo;
+    @ManyToOne //relacion de uno a muchos
+    @JoinColumn(name="codpro",nullable = false)
+    private ProductoEntity producto;
+    @ManyToOne //relacion de uno a muchos
+    @JoinColumn(name="codcat",nullable = false)
+    private CategoriaEntity categoria;
+    @ManyToOne //relacion de uno a muchos
+    @JoinColumn(name="codent",nullable = false)
+    private EntradaEntity entrada;
+    @ManyToOne //relacion de uno a muchos
+    @JoinColumn(name="codentde",nullable = false)
+    private EntradaDetalleEntity entradadetalle;
+     
 }
+    
+
