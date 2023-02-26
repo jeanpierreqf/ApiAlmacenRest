b5a7ccec4579a6453114a37ecf35cab9ffb431fd
BryanErnesto27 <117932365+BryanErnesto27@users.noreply.github.com>
2023-02-25 23:07:34 -0600

    frederick

--- src/main/java/pe/com/apialmacen/service/impl/gestion/EntradaDetalleServiceImpl.java
+++ src/main/java/pe/com/apialmacen/service/impl/gestion/EntradaDetalleServiceImpl.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import pe.com.apialmacen.entity.gestion.EntradaDetalleEntity;
 import pe.com.apialmacen.repository.gestion.EntradaDetalleRepository;
 import pe.com.apialmacen.service.gestion.EntradaDetalleService;
 
+@Service
 public class EntradaDetalleServiceImpl implements EntradaDetalleService{
     
     @Autowired
