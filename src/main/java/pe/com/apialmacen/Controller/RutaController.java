f84dc03450f2c1a9156db2c9bf53d1a467ade41a
belli <AdrianBellidoPozo>
2023-02-26 21:47:04 -0500

    Creacion de html

--- src/main/java/pe/com/apialmacen/Controller/RutaController.java
+++ src/main/java/pe/com/apialmacen/Controller/RutaController.java
@@ -16,12 +16,47 @@ public class RutaController {
     }
 
     @GetMapping("/mostrarproducto")
-    public String MostrarInicioProducto() {
+    public String MostrarProducto() {
         return "/producto/mostrarproducto";
     }
 
     @GetMapping("/principal")
     public String MostrarMenuPrincipal() {
         return "principal";
+    }
+    
+    @GetMapping("/entradaDetalle")
+    public String MostrarEntradaDetalle() {
+        return "entradaDetalle/entradaDetalle";
+    }
+    
+    @GetMapping("/entrada")
+    public String MostrarEntrada() {
+        return "entrada/entrada";
+    }
+    
+    @GetMapping("/proveedor")
+    public String MostrarProveedor() {
+        return "proveedor/proveedor";
+    }
+    
+    @GetMapping("/roles")
+    public String MostrarRoles() {
+        return "roles/roles";
+    }
+    
+    @GetMapping("/salidaDetalle")
+    public String MostrarSalidaDetalle() {
+        return "salidaDetalle/salidaDetalle";
+    }
+    
+    @GetMapping("/salida")
+    public String MostrarSalida() {
+        return "salida/salida";
+    }
+    
+    @GetMapping("/usuarios")
+    public String MostrarUsuarios() {
+        return "usuarios/usuarios";
     }
 }
