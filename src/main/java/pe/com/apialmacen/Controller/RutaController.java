e89a470a88a71ac55d7ac7c8695e454284de13bf
JuniorCerpe <juniorcervera13@gmail.com>
2023-02-27 11:48:26 -0500

    templates

--- src/main/java/pe/com/apialmacen/Controller/RutaController.java
+++ src/main/java/pe/com/apialmacen/Controller/RutaController.java
@@ -19,6 +19,38 @@ public class RutaController {
     public String MostrarProducto() {
         return "/producto/mostrarproducto";
     }
+    @GetMapping("/mostrarproveedor")
+    public String MostrarProveedor() {
+        return "/proveedor/mostrarproveedor";
+    }
+    @GetMapping("/mostrarroles")
+    public String MostrarRoles() {
+        return "/roles/mostrarroles";
+    }
+    @GetMapping("/mostrarusuarios")
+    public String MostrarUsuarios() {
+        return "/usuarios/mostrarusuarios";
+    }
+    @GetMapping("/mostrarentrada")
+    public String MostrarEntrada() {
+        return "/entrada/mostrarentrada";
+    }
+    
+    @GetMapping("/mostrarentradadetalle")
+    public String MostrarEntradaDetalle() {
+        return "/entradadetalle/mostrarentradadetalle";
+    }
+    
+    @GetMapping("/mostrarsalida")
+    public String MostrarSalida() {
+        return "/salida/mostrarsalida";
+    }
+    
+    @GetMapping("/mostrarsalidadetalle")
+    public String MostrarSalidaDetalle() {
+        return "/salidadetalle/mostrarsalidadetalle";
+    }
+   
 
     @GetMapping("/principal")
     public String MostrarMenuPrincipal() {
